f0fc7bf25a00632f784c26a44df001800a84ccf1
detrna <himselfunderscore@gmail.com>
2026-02-24 12:35:05 +0700

    Implementation fix

--- LK01-Latihan tentang class, obyek dan inheritance/App.java
+++ LK01-Latihan tentang class, obyek dan inheritance/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) {
-        bujurSangkar bujurSangkar1 = new bujurSangkar("Bujur Sangkar 1", 4);
-        lingkaran lingkaran1 = new lingkaran("Lingkaran 1", 4);
+        bidangDuaDimensi bujurSangkar1 = new bujurSangkar("Bujur Sangkar 1", 4);
+        bidangDuaDimensi lingkaran1 = new lingkaran("Lingkaran 1", 4);
 
         bujurSangkar1.luas();
         bujurSangkar1.info();
--- LK01-Latihan tentang class, obyek dan inheritance/bidangDuaDimensi.java
+++ LK01-Latihan tentang class, obyek dan inheritance/bidangDuaDimensi.java
@@ -12,15 +12,3 @@ public class bidangDuaDimensi{
        
     }
 }
-
-/*
-Superclass juga mempunyai method info() dan method luas().
-Method Info() di superclass akan mencetak nilai atribut nama
-Catatan: Method luas() belum di definisikan
-Buat sub class bujurSangkar yang mempunyai atribut sisi
-Sub class bujurSangkar adalah sub class dari super class bidangDuaDimensi
-Buat contructor dari class bujurSangkar
-Lakukan overriding dari method luas() yang berada di super class
-Seperti saat membangun sub class bujurSangkar, buat sub class Lingkaran
-Implementasikan di kelas App
-*/
